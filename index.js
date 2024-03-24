b5b1a0f8aaaeb9775bdba648698f7b18d4856152
Neha <nehamkadam2002>
2024-03-24 20:46:19 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function fetchData() {
-    const userId_Input = document.getElementById('userIdInput').value;
+    const userId_Input = document.getElementById('userId_Input').value;
     const url = `https://reqres.in/api/users/${userId_Input}`;
-    const tableData = document.getElementById('userlist');
+    const table = document.getElementById('userlist');
     const spinnerContainer = document.querySelector(".spinner-container");
 
     spinnerContainer.style.display = "block";
-    tableData.style.display = "none";
+    table.style.display = "none";
 
     fetch(url)
         .then(response => {
@@ -15,18 +15,28 @@ function fetchData() {
             return response.json();
         })
         .then(data => {
-            tableData.innerHTML = '';
-            const row = document.createElement('tr');
-            row.innerHTML = `
+            table.innerHTML = '';
+            const headingsRow = document.createElement('tr');
+            headingsRow.innerHTML = `
+                <th>ID</th>
+                <th>Avatar</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+            `;
+            table.appendChild(headingsRow);
+            const userRow = document.createElement('tr');
+            userRow.innerHTML = `
                 <td>${data.data.id}</td>
                 <td><img src="${data.data.avatar}" alt="avatar" width="50"></td>
                 <td>${data.data.first_name}</td>
                 <td>${data.data.last_name}</td>
                 <td>${data.data.email}</td>
             `;
-            tableData.appendChild(row);
+            table.appendChild(userRow);
+
             spinnerContainer.style.display = "none";
-            tableData.style.display = "table";
+            table.style.display = "table";
         })
         .catch(error => {
             console.error('Error fetching data:', error);
